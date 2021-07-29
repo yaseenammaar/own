9b0ab8dab864fc8a7b3ae8377d123a8af00d8973
yaseenammaar <yammaar9@gmail.com>
2021-07-29 11:01:03 +0530

    Cool name function added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import SpacingGrid from './columns.js';
+import variants from './variants.json';
 
+
+
+function getCoolNames(domain){
+  var names = "";
+  console.log("Domain : ", domain)
+  if(domain.includes('o')||domain.includes('O')){
+    console.log(variants["O"]);
+    variants["O"].forEach(v=>{
+      console.log("Cool Domains : ", v)
+      names = names + ", "+ domain.replace(/o/g, v);
+    })
+  }
+  console.log("Cool Domains : ", names)
+  return names;
+}
+
+getCoolNames("google.com")
+
+
+// console.log(variants);
 
 
 function App() {
