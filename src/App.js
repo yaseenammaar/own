0763e4c85e960415afbb2ea130f908e7cb4b5e5d
yaseenammaar <yammaar9@gmail.com>
2021-07-30 09:29:56 +0530

    completed function

--- src/App.js
+++ src/App.js
@@ -2,21 +2,69 @@ import logo from './logo.svg';
 import './App.css';
 import variants from './variants.json';
 import SpacingGrid from './components/columns.js';
+const vowels = ["a", "e", "i", "o", "u", "y"];
+
+
+
+function isVowel(word) {
+    let result = false;
+    let split = word.split('.');
+    console.log(split[0])
+    for (let i = 0; i < split[0].length; i++) {
+        if (vowels.includes(split[0][i])) {
+            getCoolNames(split[0])
+            // result = true;
+            // break;
+        }
+    }
+    // console.log(result)
+    return result;
+  }
+
 
 
 function getCoolNames(domain){
   var names = "";
   console.log("Domain : ", domain)
+
   if(domain.includes('o')||domain.includes('O')){
-    console.log(variants["O"]);
     variants["O"].forEach(v=>{
-      console.log("Cool Domains : ", v)
       names = names + ", "+ domain.replace(/o/g, v);
     })
   }
+
+  if(domain.includes('a')||domain.includes('A')){
+    variants["A"].forEach(v=>{
+      names = names + ", "+ domain.replace(/a/g, v);
+    })
+  }
+
+  if(domain.includes('e')||domain.includes('E')){
+    variants["E"].forEach(v=>{
+      names = names + ", "+ domain.replace(/e/g, v);
+    })
+  }
+
+  if(domain.includes('i')||domain.includes('I')){
+    variants["I"].forEach(v=>{
+      names = names + ", "+ domain.replace(/i/g, v);
+    })
+  }
+
   console.log("Cool Domains : ", names)
   return names;
 }
+
+
+isVowel("ggl.com");
+isVowel("yahoo.com");
+isVowel("amazon.com");
+isVowel("oyo.com");
+isVowel("fb.com");
+isVowel("airbnb.com");
+isVowel("lewis.com");
+isVowel("puma.com");
+isVowel("nike.com");
 
 getCoolNames("google.com")
 
